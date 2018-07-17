c528f55a2f03a34448661b765e0a071ebcd58af2
Matheus Santana <embs@cin.ufpe.br>
2018-07-17 16:00:59 -0300

    cpuplot: print table data

--- cpuplot.r
+++ cpuplot.r
@@ -4,6 +4,16 @@ library(ggplot2)
 
 pdf(NULL) # Avoid creating Rplots.pdf file
 
+print_table_data <- function(no_tracing, instrumented, rbinder) {
+  means <- c(round(mean(no_tracing),   digits = 2),
+             round(mean(instrumented), digits = 2),
+             round(mean(rbinder)     , digits = 2))
+  overhead <- c(0,
+                round(means[2] / means[1], digits = 2),
+                round(means[3] / means[1], digits = 2))
+  print(data.frame(means, overhead))
+}
+
 scenarios <- sub('.log', '', list.files('./cpu'))
 plotsdir <- './plots/'
 dir.create(plotsdir, showWarnings=FALSE)
@@ -11,6 +21,8 @@ dir.create(plotsdir, showWarnings=FALSE)
 no_tracing <- read.csv(file='./cpu/no_tracing.log', head=FALSE)$V1
 instrumented <- read.csv(file='./cpu/instrumented.log', head=FALSE)$V1
 rbinder <- read.csv(file='./cpu/rbinder.log', head=FALSE)$V1
+
+print_table_data(no_tracing, instrumented, rbinder)
 
 # Aggregate data.
 reps <- rep(1:(length(no_tracing)/60), each=60)
